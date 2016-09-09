7617b98d1c1dddd8c8ffd6cb5315fbff6654b792
Jim Caratozzolo <jcaratozzolo@auction.com>
2016-09-09 01:48:26 -0700

    big ole commit

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ app.get('/slides', function (req, res) { res.redirect('/slides/01') })
 app.get('/slides/01', function (req, res) { res.render('slides/s01') })
 app.get('/slides/02', function (req, res) { res.render('slides/s02') })
 app.get('/slides/03', function (req, res) { res.render('slides/s03') })
+app.get('/slides/04', function (req, res) { res.render('slides/s04') })
+app.get('/slides/05', function (req, res) { res.render('slides/s05') })
+app.get('/slides/06', function (req, res) { res.render('slides/s06') })
+app.get('/slides/07', function (req, res) { res.render('slides/s07') })
 
 var server = app.listen(8089, function () {
   var port = server.address().port
-  console.log('App listening at %s', port)
+  console.log('App listening at http://localhost/%s', port)
 })
